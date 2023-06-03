15a87601638606e3c672f17b8fa21ad32f56869b
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2023-06-03 16:49:02 +0200

    fixed search bar (now is possible to display when characters deleting)

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -7,6 +7,7 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
@@ -26,6 +27,12 @@ class App extends Component {
   }
 
   render() {
+    console.log("render");
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className="App">
         <input
@@ -34,16 +41,13 @@ class App extends Component {
           placeholder={"Search monsters"}
           onChange={(event) => {
             console.log(event.target.value);
-            const searchString = event.target.value.toLocaleLowerCase();
-            const filteredMonsters = this.state.monsters.filter((monster) => {
-              return monster.name.toLocaleLowerCase().includes(searchString);
-            });
+            const searchField = event.target.value.toLocaleLowerCase();
             this.setState(() => {
-              return { monsters: filteredMonsters };
+              return { searchField };
             });
           }}
         />
-        {this.state.monsters.map((monster) => {
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
