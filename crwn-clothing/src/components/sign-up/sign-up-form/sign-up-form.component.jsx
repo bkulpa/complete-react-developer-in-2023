711f1ac77dc9bf1635d741156f249b56d8f7ff96
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2023-08-10 20:56:06 +0200

    Added sign-up handler

--- crwn-clothing/src/components/sign-up/sign-up-form/sign-up-form.component.jsx
+++ crwn-clothing/src/components/sign-up/sign-up-form/sign-up-form.component.jsx
@@ -2,11 +2,6 @@ import { useState } from "react";
 
 import { createAuthUserWithEmailAndPassword } from "../../../utils/firebase/firebase.utils";
 
-const handleSubmit = async (event) => {
-  //dokonczyc dodawanie nowego usera do bazy
-  event.preventDefault();
-};
-
 const defaultFormFields = {
   displayName: "",
   email: "",
@@ -19,6 +14,25 @@ const SignUpForm = () => {
   const { displayName, email, password, confirmPassword } = formFields;
 
   console.log(formFields);
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (password !== confirmPassword) {
+      alert("Password do not match");
+      return;
+    }
+
+    try {
+      const response = await createAuthUserWithEmailAndPassword(
+        email,
+        password
+      );
+      console.log(response);
+    } catch (error) {
+      console.log("user creation encountered an error", error);
+    }
+  };
 
   const handleChange = (event) => {
     const { name, value } = event.target;
