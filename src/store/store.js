713840db6b104fcae90f22f5cf73dff3ba9b68d6
Bartosz Kulpa <bartek.kulpa95@gmail.com>
2023-11-24 00:11:12 +0100

    added redux for user context

--- src/store/store.js
+++ src/store/store.js
@@ -2,7 +2,6 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-import { compileString } from "sass";
 
 const middleWares = [logger];
 
